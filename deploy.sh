ac82c5d20efce03b2116c15e81ab7f7f3483b96c
Kendrick Coleman <kendrickcoleman@gmail.com>
2015-12-09 15:21:29 -0500

    massive update and adding travis
    pray that it works this time :)
    
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+lastCommit=$(git log --format="%H" -n 1)
+filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
+for f in $filesChanged
+do
+ if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+ then
+ 	echo "Uploading $f"
+ 	curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bourbonpursuit.com/citest/$f
+ fi
+done
