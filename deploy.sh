fe392ffa978af81e9fa843787294a6f70ceedc71
Kendrick Coleman <kendrickcoleman@gmail.com>
2015-12-09 15:57:58 -0500

    updated deployment script
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 lastCommit=$(git log --format="%H" -n 1)
 filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
-for f in $filesChanged
-do
- if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
- then
- 	echo "Uploading $f"
- 	curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bourbonpursuit.com/citest/$f
- fi
-done
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update"
+else
+    for f in $filesChanged
+	do
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+		then
+	 		echo "Uploading $f"
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bourbonpursuit.com/citest/$f
+		fi
+	done
+fi
