e130101f496662ce30d7fdac8f37293888c16bdb
Kendrick Coleman <kendrickcoleman@gmail.com>
2015-12-09 19:08:31 -0500

    final travis update
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ else
 		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
 		then
 	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bourbonpursuit.com/citest/$f
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bourbonpursuit.com/$f
 		fi
 	done
 fi
